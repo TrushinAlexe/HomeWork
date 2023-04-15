b9aeb32e4e29a934b09af75d037b00da361745f5
Aleksey A.A <c1d1o@yandex.ru>
2023-04-15 20:59:13 +0300

    add Task 4

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -51,3 +51,18 @@ else
 }
 */
 
+// Task 4.Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+/*
+Console.WriteLine("Enter a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+while(current<=num)
+{
+  if(current%2==0)
+  Console.WriteLine(current+" ");
+  current++;
+}
+*/

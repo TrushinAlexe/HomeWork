946c1dfc23a56b06fcb55512c4fa71f5f876faa1
Aleksey A.A <c1d1o@yandex.ru>
2023-04-15 15:29:23 +0300

    Lection 1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,36 @@
+﻿//Seminar1
+
+// Task 1. Напишите программу, которая на вход принимает два числа и проверяет, 
+// является ли первое число квадратом второ
+/*
+Console.Write("Inpud a first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Inpud a second number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+int quad2 = num2 * num2;
+if (num1 == quad2)
+{
+  Console.WriteLine("Yes! ");
+}
+else
+{
+  Console.WriteLine("No! ");
+}
+*/
+
+// Task 2. Напишите программу которая на вход принимает одно число (N),
+// а на выходе показывает все целые числа в промежутке от -N до N.
+
+/*
+Console.Write("Inpud a first number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int current = number * -1;
+
+while(current <= number)
+{
+  Console.Write(current + " ");
+  current++; // current = current + 1
+}
+*/
